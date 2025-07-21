25a157180b3a64bdd3ab82bcb1459a89724863d1
LeonardoGB29 <lgaonab@unsa.edu.pe>
2025-07-21 04:00:36 -0500

    agregando la logica del documento (acta)

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from extensions import db, migrate
+from controllers.document_controller import document_bp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "123456")
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+from .presentation.vital.document_controller import document_bp
+app.register_blueprint(document_bp, url_prefix="/register_vital")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
